77a958dff13a93b29e39dcac7001e45c225f7185
Ezedin Fedlu <ezedin.fedlu@gmail.com>
2022-09-16 19:16:54 +0300

    update

--- deployment/k8s/deploy.sh
+++ deployment/k8s/deploy.sh
@@ -4,14 +4,14 @@ set -e
 
 kubectl apply -f env-configmap.yaml
 
-kubectl apply -f env-secret.yaml
+# kubectl apply -f env-secret.yaml
 
-kubectl apply -f aws-secret.yaml
+# kubectl apply -f aws-secret.yaml
 
-kubectl apply -f backend-feed-deployment.yaml
+# kubectl replace --force -f backend-feed-deployment.yaml
 
-kubectl apply -f backend-user-deployment.yaml
+# kubectl replace --force -f backend-user-deployment.yaml
 
-kubectl apply -f reverseproxy-deployment.yaml
+# kubectl apply -f reverseproxy-deployment.yaml
 
-kubectl apply -f frontend-deployment.yaml
+# kubectl apply -f frontend-deployment.yaml
